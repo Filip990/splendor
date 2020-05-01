6939fedcac5ee7f6c60b6d465405378bf7f4afbd
Filip Filipovic <filipfilipovic.stray@gmail.com>
2020-05-01 19:12:47 +0200

    resolved conflict

--- src/components/Konva/Stage/Stage.js
+++ src/components/Konva/Stage/Stage.js
@@ -1,24 +1,25 @@
-
-import React, { useRef } from 'react';
-import { Stage, Layer } from 'react-konva';
-import Card from "../Card/Card";
+import React, { useRef } from "react";
+import { Stage, Layer } from "react-konva";
 import Chip from "../Chip/Chip";
-import {CARD, CHIP_SETUP, STAGE_CONTAINER} from "../../../utils/enums";
+import { CHIP_SETUP, STAGE_CONTAINER } from "../../../utils/enums";
 
 import ChipsContainer from "../ChipsContainer/ChipsContainer";
 import CardContainer from "../CardContainer/CardContainer";
 
-import { CARD } from "../../../utils/enums";
-
 const StageContainer = () => {
   const stageRef = useRef(null);
 
-
   return (
     <div id={STAGE_CONTAINER}>
-      <Stage width={window.innerWidth} height={window.innerHeight} ref={stageRef}>
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        ref={stageRef}
+      >
         <Layer>
-     {Object.keys(CHIP_SETUP).map(name => <Chip name={name} />)}
+          {Object.keys(CHIP_SETUP).map((name) => (
+            <Chip name={name} />
+          ))}
           {/* main game container */}
           <CardContainer />
           {/* chips container */}
