3ed4065037e9cae05be998698d0490a4d426bc9d
srecko-jarcevic <srecko.jarcevic@symphony.is>
2020-05-01 18:41:16 +0200

    added chips

--- src/utils/enums.js
+++ src/utils/enums.js
@@ -4,3 +4,35 @@ export const CARD = {
     height: 250,
     name: 'Card',
 };
+export const CHIP = {
+    radius: 50,
+};
+export const CHIP_COLOR = {
+    BLACK: 'black',
+    RED: 'red',
+    BLUE: 'blue',
+    GREEN: 'green',
+    WHITE: 'white',
+    GOLD: 'yellow',
+};
+
+export const CHIP_SETUP = {
+    [CHIP_COLOR.BLACK]: {
+        quantity: 6
+    },
+    [CHIP_COLOR.RED]: {
+        quantity: 6
+    },
+    [CHIP_COLOR.BLUE]: {
+        quantity: 6
+    },
+    [CHIP_COLOR.GREEN]: {
+        quantity: 6
+    },
+    [CHIP_COLOR.WHITE]: {
+        quantity: 6
+    },
+    [CHIP_COLOR.GOLD]: {
+        quantity: 6
+    },
+};
