d1e427908b6d145fc7b91378c01d0530ce4f4a9a
Filip Filipovic <filipfilipovic.stray@gmail.com>
2020-05-02 18:43:05 +0200

    Added royals

--- src/components/Konva/ChipsContainer/ChipsContainer.js
+++ src/components/Konva/ChipsContainer/ChipsContainer.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Group } from "react-konva";
-import {CHIP_SETUP} from "../../../utils/constants";
+import { CHIP_SETUP } from "../../../utils/constants";
 import Chip from "../Chip/Chip";
 
 const ChipsContainer = () => {
-  const chips = Object.keys(CHIP_SETUP).map(name => <Chip name={name} />);
+  const chips = Object.keys(CHIP_SETUP).map((name) => (
+    <Chip key={name} name={name} />
+  ));
 
   return (
-    <Group
-      x={window.innerWidth - 300}
-      y={130}
-    >
+    <Group x={window.innerWidth - 200} y={130}>
       {chips}
     </Group>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,56 +1,57 @@
-export const STAGE_CONTAINER = 'stageContainer';
+export const STAGE_CONTAINER = "stageContainer";
 export const CARD_COLOR = {
-    GREEN: 'green',
-    BLUE: 'blue',
-    YELLOW: 'yellow',
+  ROYAL: "red",
+  GREEN: "green",
+  BLUE: "blue",
+  YELLOW: "yellow",
 };
 export const CARD_TYPE = {
-    [CARD_COLOR.GREEN]: {
-        quantity: 20,
-    },
-    [CARD_COLOR.BLUE]: {
-        quantity: 20,
-    },
-    [CARD_COLOR.YELLOW]: {
-        quantity: 20,
-    },
+  [CARD_COLOR.ROYAL]: {
+    quantity: 10,
+  },
+  [CARD_COLOR.GREEN]: {
+    quantity: 20,
+  },
+  [CARD_COLOR.BLUE]: {
+    quantity: 20,
+  },
+  [CARD_COLOR.YELLOW]: {
+    quantity: 20,
+  },
 };
 export const CARD_HEIGHT = window.innerHeight / 6;
-export const CARD = {
-    width: 80,
-    height: CARD_HEIGHT,
-    name: 'Card',
-};
+export const CARD_WIDTH = window.innerWidth / 15;
+
 export const CHIP_RADIUS = window.innerHeight / 30;
 export const CHIP = {
-    radius: CHIP_RADIUS,
+  radius: CHIP_RADIUS,
 };
 export const CHIP_COLOR = {
-    BLACK: 'black',
-    RED: 'red',
-    BLUE: 'blue',
-    GREEN: 'green',
-    WHITE: 'white',
-    GOLD: 'yellow',
+  BLACK: "black",
+  RED: "red",
+  BLUE: "blue",
+  GREEN: "green",
+  WHITE: "white",
+  GOLD: "yellow",
 };
 
 export const CHIP_SETUP = {
-    [CHIP_COLOR.BLACK]: {
-        quantity: 6
-    },
-    [CHIP_COLOR.RED]: {
-        quantity: 6
-    },
-    [CHIP_COLOR.BLUE]: {
-        quantity: 6
-    },
-    [CHIP_COLOR.GREEN]: {
-        quantity: 6
-    },
-    [CHIP_COLOR.WHITE]: {
-        quantity: 6
-    },
-    [CHIP_COLOR.GOLD]: {
-        quantity: 6
-    },
+  [CHIP_COLOR.BLACK]: {
+    quantity: 6,
+  },
+  [CHIP_COLOR.RED]: {
+    quantity: 6,
+  },
+  [CHIP_COLOR.BLUE]: {
+    quantity: 6,
+  },
+  [CHIP_COLOR.GREEN]: {
+    quantity: 6,
+  },
+  [CHIP_COLOR.WHITE]: {
+    quantity: 6,
+  },
+  [CHIP_COLOR.GOLD]: {
+    quantity: 6,
+  },
 };
